edc9d6dffdc611009fe57d324809559566f7c741
Ryuichi Miyazaki <rmiyazaki11@ucsbalum.com>
2020-03-28 23:58:50 -0700

    Creating new repo for Angular tutorial

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. 0' },
+  { id: 12, name: 'Miss All-Sunday' },
+  { id: 13, name: 'Mr. 1' },
+  { id: 14, name: 'Miss Doublefinger' },
+  { id: 15, name: 'Mr. 2 Bon Kurei' },
+  { id: 16, name: 'Mr. 3' },
+  { id: 17, name: 'Miss Goldenweek' },
+  { id: 18, name: 'Mr. 4' },
+  { id: 19, name: 'Miss Merry Christmas' },
+  { id: 20, name: 'Mr. 5' },
+  { id: 21, name: 'Miss Valentine' }
+];
